50950c3e9ec098387842260a30dedc0902e2d18e
yuittti <kozachenkos@gmail.com>
2018-08-21 18:59:54 +0300

    Add basic reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import users from './users';
+import polls from './polls';
+import authedUser from './authedUser';
+
+
+export default combineReducers({
+    users,
+    polls,
+    authedUser,
+});
