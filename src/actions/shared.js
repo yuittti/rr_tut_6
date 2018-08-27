a6933d5f0f0bdc19efb51863a85e4e1aedace447
yuittti <kozachenkos@gmail.com>
2018-08-27 16:13:48 +0300

    Add Loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,17 +1,20 @@
 import { getInitialData } from '../utils/api';
 import { receivePolls } from '../actions/polls';
 import { receiveUsers } from '../actions/users';
-import {setAuthedUser} from './authedUser';
+import { setAuthedUser } from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'sarah_edo';
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading());
         return getInitialData()
             .then(({ users, polls }) => {
                 dispatch(receiveUsers(users));
                 dispatch(receivePolls(polls));
                 dispatch(setAuthedUser(AUTHED_ID));
+                dispatch(hideLoading());
             })
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import users from './users';
 import polls from './polls';
 import authedUser from './authedUser';
+import { loadingBarReducer } from 'react-redux-loading';
 
 
 export default combineReducers({
     users,
     polls,
     authedUser,
+    loadingBar: loadingBarReducer,
 });
